cb3dd517edc8f24ee57693ce54d253c0d2e229e5
Hemalatha <hemachowdary2744@gmail.com>
2024-03-14 20:19:44 +0530

    commit

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,5 +1,12 @@
+import Youtube from "./Youtube";
+
 function App() {
-  return <h1>classic react app</h1>;
+  return (
+    <>
+      <h1>classic react app</h1>;
+      <Youtube />
+    </>
+  );
 }
 
 export default App;
