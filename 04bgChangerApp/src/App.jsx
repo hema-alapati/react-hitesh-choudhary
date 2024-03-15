89d936b51b06037df7f0b7e4da747272f7ad8783
Hemalatha <hemachowdary2744@gmail.com>
2024-03-15 23:04:21 +0530

    colorChanger

---
+++ 04bgChangerApp/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [color, setColor] = useState("olive");
+
+  function changeColor(color) {
+    setColor(color);
+  }
+
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 insert-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => changeColor("red")}
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg "
+          >
+            red
+          </button>
+          <button
+            onClick={() => changeColor("green")}
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg "
+          >
+            green
+          </button>
+          <button
+            onClick={() => changeColor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg "
+          >
+            blue
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
